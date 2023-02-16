dd787d3628b666ed054580b7540feae1e87bd207
gname <gname.koita@yahoo.fr>
2023-02-16 11:39:14 +0100

    style.css

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require "lib/base-de-donne.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>recettes_cuisine</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="style.css" href="">
 </head>
 <body>
 
